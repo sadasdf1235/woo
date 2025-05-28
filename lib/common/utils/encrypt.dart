4794094c5d9e40c804293f3a0b5a63d97027947b
sadasdf1235 <tgb80576@163.com>
2025-05-28 12:17:26 +0800

    feat: 密码AES加密后传输

--- lib/common/utils/encrypt.dart
+++ lib/common/utils/encrypt.dart
@@ -1,26 +1,33 @@
 import 'package:encrypt/encrypt.dart';
 
-import '../values/constants.dart';
+import '../index.dart';
 
 /// 加密类
 class EncryptUtil {
   static final EncryptUtil _instance = EncryptUtil._internal();
   factory EncryptUtil() => _instance;
   EncryptUtil._internal() {
+    // 加密器
     encrypter = Encrypter(AES(
-      key,
-      mode: AESMode.cbc,
-      padding: 'PKCS7',
+      key, // 密钥
+      mode: AESMode.cbc, // 模式
+      padding: 'PKCS7', // 填充模式
     ));
   }
 
+  /// 密钥
   final key = Key.fromUtf8(Constants.aesKey);
+
+  /// 偏移量
   final iv = IV.fromUtf8(Constants.aesIV);
+
   late Encrypter encrypter;
 
   /// aes加密
   String aesEncode(String content) {
+    // 加密
     final encrypted = encrypter.encrypt(content, iv: iv);
+    // 返回 base64 编码
     return encrypted.base64;
   }
 }
