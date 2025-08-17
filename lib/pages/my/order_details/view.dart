a7ea26967373224d701567665f755ad0f1a17b3c
sadasdf1235 <tgb80576@163.com>
2025-08-17 15:25:39 +0800

    feat: 费用小计

--- lib/pages/my/order_details/view.dart
+++ lib/pages/my/order_details/view.dart
@@ -172,7 +172,64 @@ class OrderDetailsPage extends GetView<OrderDetailsController> {
 
   // 小计
   Widget _buildTotal() {
-    return const Text("小计");
+    return <Widget>[
+      // 左侧
+      <Widget>[
+        // Payment Method
+        TextWidget.label(
+          LocaleKeys.orderDetailsPaymentMethod.tr,
+          weight: FontWeight.bold,
+        ),
+
+        // VISA Card Payment
+        const TextWidget.label("VISA Card Payment"),
+
+        // Balance 余额
+        BuildTotalItem(
+          title: LocaleKeys.orderDetailsBalance.tr,
+          currencySymbol: controller.order.currencySymbol,
+          price: "0",
+        ),
+      ]
+          .toColumn(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.start,
+          )
+          .expanded(),
+
+      // 间距
+      SizedBox(
+        width: AppSpace.iconTextMedium,
+      ),
+
+      // 右侧
+      <Widget>[
+        // Total
+        BuildTotalItem(
+          title: LocaleKeys.orderDetailsTotal.tr,
+          currencySymbol: controller.order.currencySymbol,
+          price: controller.order.total,
+        ),
+
+        // Shipping
+        BuildTotalItem(
+          title: LocaleKeys.orderDetailsShipping.tr,
+          currencySymbol: controller.order.currencySymbol,
+          price: controller.order.shippingTotal,
+        ),
+
+        // Discount
+        BuildTotalItem(
+          title: LocaleKeys.orderDetailsDiscount.tr,
+          currencySymbol: controller.order.currencySymbol,
+          price: controller.order.discountTotal,
+        ),
+      ]
+          .toColumn(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+          )
+          .expanded(),
+    ].toRow().height(100).paddingAll(AppSpace.card).card();
   }
 
   // 主视图
--- lib/pages/my/order_details/widget/index.dart
+++ lib/pages/my/order_details/widget/index.dart
@@ -2,3 +2,4 @@ library widget;
 
 export './bill_address.dart';
 export './products_list.dart';
+export './total_item.dart';
